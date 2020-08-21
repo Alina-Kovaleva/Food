d72ecfd3a751c3a246b906f5bd3fef4772623fcc
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2020-08-21 17:14:35 +0200

    added a function that determines the difference between deadline and current time.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items ');
@@ -37,4 +39,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //Timer
+
+    const deadline = '2020-08-22';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds 
+        };
+    }
+    
 });
