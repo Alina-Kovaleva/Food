c919e1e851146e863449bcfd37447610c934cb92
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2020-08-21 17:37:54 +0200

    added a function that sets the timer directly to the page

--- js/script.js
+++ js/script.js
@@ -58,5 +58,27 @@ document.addEventListener('DOMContentLoaded', () => {
             'seconds': seconds 
         };
     }
-    
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
 });
