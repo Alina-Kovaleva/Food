186e16a69f8dc46604ff4142719659f7bffe47ef
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2020-09-01 14:43:00 +0200

    removing extra code

--- js/script.js
+++ js/script.js
@@ -286,9 +286,4 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
-    
 });
