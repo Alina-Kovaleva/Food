b7f45cb092b8c832736fc513da0646771285c42a
Alina Kovaleva <a.d.kovaleva@yandex.ru>
2020-09-02 15:49:55 +0200

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -503,22 +503,24 @@ document.addEventListener('DOMContentLoaded', () => {
     function getStaticInformation(parentSelector, activeClass) {
         const elements = document.querySelectorAll(`${parentSelector} div`);
 
-        document.querySelector(parentSelector).addEventListener('click', (e) => {
-            if (e.target.getAttribute('data-ratio')) {
-                ratio = +e.target.getAttribute('data-ratio');
-            } else {
-                sex = e.target.getAttribute('id');
-            }
-
-            console.log(ratio, sex);
-
-            elements.forEach(elem => {
-                elem.classList.remove(activeClass);
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+    
+                console.log(ratio, sex);
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass);
+    
+                calcTotal();
             });
-
-            e.target.classList.add(activeClass);
-
-            calcTotal();
         });
     }
 
